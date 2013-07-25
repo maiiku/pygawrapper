fa4db879c666cb09c0a909ccf3f35d40cd3489e2
mko_san <mononoke>
2013-07-25 12:30:09 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+#with open('README.rst') as f:
+#    long_description = f.read()
+
+setup(
+    name='pygawrapper',
+    description='pyga wrapper for django',
+    long_description='pyga wrapper for django',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/maiiku/pygawrapper.git',
+    license='MIT',
+    author='mko_san',
+    author_email='mko_san@lafiel.net',
+
+)
