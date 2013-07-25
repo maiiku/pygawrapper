c190ca7cf7604525a03886522a1f8220307d8950
mko_san <mononoke>
2013-07-25 16:21:59 +0200

    improvements

--- pygawrapper/mixins.py
+++ pygawrapper/mixins.py
@@ -6,27 +6,32 @@ class PygaMixin(object):
     Add function to retrive ga data.
     """
     def get_utma(self, user_id, *args, **kwargs):
-        ga = Pygawrapper.objects.get_or_create(user_id=user_id)
-        self._utma = StringCookieJar(ga.utma)._cookies
+        if not hasattr(self, '_utma'):
+            ga = Pygawrapper.objects.get_or_create(user_id=user_id)
+            self._utma = StringCookieJar(ga.utma)._cookies
         return self._utma
 
     def get_utmb(self, user_id, *args, **kwargs):
-        ga = Pygawrapper.objects.get_or_create(user_id=user_id)
-        self._utmb = StringCookieJar(ga.utmb)._cookies
+        if not hasattr(self, '_utmb'):
+            ga = Pygawrapper.objects.get_or_create(user_id=user_id)
+            self._utmb = StringCookieJar(ga.utmb)._cookies
         return self._utmb
 
     def get_ga_visitor(self, *args, **kwargs):
-        self.ga_visitor = Visitor().extract_from_utma(self._utma)
+        if not hasattr(self, 'ga_visitor'):
+            self.ga_visitor = Visitor().extract_from_utma(self.get_utma(kwargs['user_id']))
         return self.ga_visitor
 
     def get_ga_session(self, *args, **kwargs):
-        self.ga_session = Session().extract_from_utmb(self._utmb)
+        if not hasattr(self, 'ga_session'):
+            self.ga_session = Session().extract_from_utmb(self.get_utmb(kwargs['user_id']))
         return self.ga_session
 
     def get_ga_tracker(self, GOOGLE_ANALYTICS_CODE, GOOGLE_ANALYTICS_SITE, user_id, *args, **kwargs):
-        self.ga_tracker = Tracker(GOOGLE_ANALYTICS_CODE, GOOGLE_ANALYTICS_SITE)
-        self.get_ga_session(user_id)
-        self.get_ga_visitor(user_id)
+        if not hasattr(self, 'ga_tracker'):
+            self.ga_tracker = Tracker(GOOGLE_ANALYTICS_CODE, GOOGLE_ANALYTICS_SITE)
+            self.get_ga_session(user_id)
+            self.get_ga_visitor(user_id)
         return self.ga_tracker
 
     def track_transaction(self, transaction):
